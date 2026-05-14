62f737023de4edb3c6aab75b8bbe5892827381d4
Firoz Khan Patan <firozkhanp009@gmail.com>
2026-05-13 20:25:14 -0400

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> container = new HashSet<>();
+
+        for(int num: nums) {
+            if(container.contains(num)) {
+                return true;
+            }
+            container.add(num);
+        }
+
+        return false;
+    }
+}
